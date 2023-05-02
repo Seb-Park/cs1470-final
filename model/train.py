833387e8ee7a197bfbfac8fbb8994e0161cc1221
crackalamoo <harysdalvi@gmail.com>
2023-05-02 11:53:24 -0400

    make convolution model

--- model/train.py
+++ model/train.py
@@ -2,7 +2,8 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Reshape, Conv2D
+from tensorflow.keras.layers import Flatten, Dense, Reshape,\
+    Conv2D, Conv2DTranspose, BatchNormalization, LeakyReLU, UpSampling2D, MaxPooling2D
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 
@@ -11,21 +12,21 @@ IMAGE_DIM = (150,100,3) # 3 color channels
 LEARNING_RATE = 1e-3
 EPOCHS = 5
 
-def load_images(directory, batch_size=128):
+def load_images(directory, batch_size=64):
     data = tf.keras.utils.image_dataset_from_directory(
         directory, labels=None, batch_size=batch_size, image_size=(IMAGE_DIM[0],IMAGE_DIM[1]),
-        seed=42
+        seed=42, validation_split=0.7, subset='training' # change this to the full dataset later
     )
     return data
 
 def train_vae(model, x_data, y_data):
     """
-    Train your VAE with one epoch.
+    Train your autoencoder with one epoch.
 
     Inputs:
     - model: Your VAE instance.
-    - x_data: A tf.data.Dataset of input images.
-    - y_data: A tf.data.Dataset of reconstructed images.
+    - x_data: A tf.data.Dataset of noisy input images (stylized images).
+    - y_data: A tf.data.Dataset of reconstructed images (photographs).
 
     Returns:
     - total_loss: Sum of loss values of all batches.
@@ -85,15 +86,15 @@ def mse_function(x, x_hat):
     reconstruction_loss = mse_fn(x, x_hat)
     return reconstruction_loss
 
+def dkl_function(mu, logvar):
+    return -0.5 * tf.math.reduce_sum(1 + logvar - tf.math.square(mu) - tf.math.exp(logvar))
+
 def bce_function(x, x_hat):
     bce_fn = tf.keras.losses.BinaryCrossentropy(
         from_logits=False, reduction=tf.keras.losses.Reduction.SUM
     )
     reconstruction_loss = bce_fn(x, x_hat)
     return reconstruction_loss
-
-def dkl_function(mu, logvar):
-    return -0.5 * tf.math.reduce_sum(1 + logvar - tf.math.square(mu) - tf.math.exp(logvar))
 
 def loss_function(x_hat, x, mu, logvar):
     """
@@ -103,16 +104,27 @@ def loss_function(x_hat, x, mu, logvar):
     Inputs:
     - x_hat: Reconstructed input data of shape (N, H, W, C)
     - x: Input data for this timestep of shape (N, H, W, C)
-    - mu: Matrix representing estimated posterior mu (N, Z), with Z latent space dimension
-    - logvar: Matrix representing estimated variance in log-space (N, Z), with Z latent space dimension
     
     Returns:
     - loss: Tensor containing the scalar loss for the negative variational lowerbound
     """
     #loss = tf.math.reduce_mean(bce_function(x, x_hat) + dkl_function(mu, logvar))
     loss = tf.math.reduce_mean(bce_function(x, x_hat))
+    #loss = tf.keras.losses.MeanSquaredError()(x, x_hat)
     return loss
 
+class Downsample(tf.keras.models.Sequential):
+    def __init__(self, filters, batchnorm=True):
+        super().__init__()
+        self.add(Conv2D(filters, 3, activation='relu', padding='same'))
+        self.add(MaxPooling2D((2, 2), padding='same'))
+        if batchnorm:
+            self.add(BatchNormalization())
+class Upsample(tf.keras.models.Sequential):
+    def __init__(self, filters):
+        super().__init__()
+        self.add(Conv2DTranspose(filters, 3, activation='tanh', padding='same'))
+        self.add(UpSampling2D((2, 2)))
 class VAE(tf.keras.Model):
     def __init__(self, image_shape, latent_size=64, hidden_dim=512):
         super(VAE, self).__init__()
@@ -122,32 +134,20 @@ class VAE(tf.keras.Model):
         self.hidden_dim = hidden_dim  # H_d
         self.mu_layer = Dense(latent_size) # not used right now
         self.logvar_layer = Dense(latent_size) # not used right now
-        self.latent_layer = Dense(latent_size)
+        self.latent_layer = Dense(latent_size) # not used right now
 
-        self.encoder = Sequential([
-            Flatten(),
-            Dense(self.hidden_dim, activation='relu'),
-            Dense(self.hidden_dim, activation='relu'),
-            Dense(self.hidden_dim, activation='relu'),
-            self.latent_layer
-        ])
+        self.enc1 = Downsample(64)
+        self.enc2 = Downsample(32)
+        self.enc3 = Downsample(16, False)
 
-        ############################################################################################
-        # self.decoder should be a network that inputs a batch of latent vectors of
-        # shape (N, Z) and outputs a tensor of estimated images of shape (N, H, W, C).
-        ############################################################################################
-        self.decoder = Sequential([
-            Dense(self.hidden_dim, activation='relu'),
-            Dense(self.hidden_dim, activation='relu'),
-            Dense(self.hidden_dim, activation='relu'),
-            Dense(self.input_size, activation='sigmoid'),
-            Reshape(self.image_shape)
-        ])
+        self.dec1 = Upsample(64)
+        self.dec2 = Upsample(32)
+        self.dec3 = Upsample(16)
+
 
     def call(self, x):
         """
-        Performs forward pass through FC-VAE model by passing image through 
-        encoder, reparametrize trick, and decoder models
+        Performs forward pass through autoencoder model
     
         Inputs:
         - x: Batch of input images of shape (N, C, H, W)
@@ -158,11 +158,15 @@ class VAE(tf.keras.Model):
         - logvar: Matrix representing estimataed variance in log-space (N, Z), with Z latent space dimension
         """
         
-        encoded = self.encoder(x)
-        #mu = self.mu_layer(encoded)
-        #logvar = self.logvar_layer(encoded)
-        #z = reparametrize(mu, logvar)
-        x_hat = self.decoder(encoded)
+        d1 = self.enc1(x)
+        d2 = self.enc2(d1)
+        d3 = self.enc3(d2)
+
+        u1 = self.dec1(d3)
+        u2 = self.dec2(u1)
+        u3 = self.dec3(u2)
+        x_hat = Conv2D(3, 3, activation='sigmoid', padding='valid')(u3)
+        x_hat = tf.keras.layers.Cropping2D((0, 1))(x_hat)
 
         return x_hat, 0, 0
 
